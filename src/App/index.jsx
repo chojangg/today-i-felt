1a3b1a9967799c2e7e400fa6785b5cc42301bb32
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-10-29 10:39:58 +0900

    에러 해결

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'; // 리액트 문법을 쓸 때 React Import
+// eslint-disable-next-line import/no-named-as-default
 import FeelingList from '../FeelingList';
 import FeelingDetail from '../FeelingDetail';
 import './style.css';
