174ee13950a7ef0edb19fcedca5bfb6b9ab28647
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-11-12 12:18:39 +0900

    RadioFeeling 파일이름 변경

--- src/AddFeeling/index.jsx
+++ src/AddFeeling/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import RadioFeeling from './RadioFeeling';
 
 class AddFeeling extends React.Component {
   render() {
@@ -11,26 +12,11 @@ class AddFeeling extends React.Component {
         <div>맑음</div>
         <div>기분</div>
         <div>
-          <label htmlFor="feeling-good">
-            <input type="radio" id="feeling-good" />
-            <img src={`${process.env.PUBLIC_URL}/images/feeling-good.svg`} alt="" />
-          </label>
-          <label htmlFor="feeling-nice">
-            <input type="radio" id="feeling-nice" />
-            <img src={`${process.env.PUBLIC_URL}/images/feeling-nice.svg`} alt="" />
-          </label>
-          <label htmlFor="feeling-disappointed">
-            <input type="radio" id="feeling-disappointed" />
-            <img src={`${process.env.PUBLIC_URL}/images/feeling-disappointed.svg`} alt="" />
-          </label>
-          <label htmlFor="feeling-angry">
-            <input type="radio" id="feeling-angry" />
-            <img src={`${process.env.PUBLIC_URL}/images/feeling-angry.svg`} alt="" />
-          </label>
-          <label htmlFor="feeling-sad">
-            <input type="radio" id="feeling-sad" />
-            <img src={`${process.env.PUBLIC_URL}/images/feeling-sad.svg`} alt="" />
-          </label>
+          <RadioFeeling feeling="good" />
+          <RadioFeeling feeling="nice" />
+          <RadioFeeling feeling="angry" />
+          <RadioFeeling feeling="disappointed" />
+          <RadioFeeling feeling="sad" />
         </div>
         <div>있었던 일</div>
         <div className="what-happened"><textarea /></div>
