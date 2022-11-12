3b0dc87c02663397b236577554fa0f19c2c685e8
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-11-12 12:00:25 +0900

    images 추가

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; // 리액트 문법을 쓸 때 React Import
 // eslint-disable-next-line import/no-named-as-default
 import FeelingList from '../FeelingList';
-import FeelingDetail from '../FeelingDetail';
+import CurrentContents from '../CurrentContents';
 import './style.css';
 
 class App extends React.Component {
@@ -9,7 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <FeelingList />
-        <FeelingDetail />
+        <CurrentContents />
       </div>
     );
   }
